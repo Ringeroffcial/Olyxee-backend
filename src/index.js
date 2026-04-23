0b441dc8447e28ecb7f2b5c559b6351129d6d724
Mahlori <ringerofficial370@gmail.com>
2026-04-23 22:19:48 +0200

    Olyxee backend

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+//testing
+let name = "Backend Engineer";
+app.get("/api/hello",(req,res)=>{
+    
+    res.status(200).json({
+        "Message":`Hello :${name}`
+    })
+})
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
