787e1dd6e5debeb5f5c8e12d0ab5de8f7e47a67a
Mahlori <ringerofficial370@gmail.com>
2026-04-24 23:17:08 +0200

    fixed Index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.get("/api/hello",(req,res)=>{
 })
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}`)
